782be5700b9bbe9d231467753524b15dfd9253ff
olihgb-7 <olihgb-7@student.ltu.se>
2021-04-14 20:44:06 +0200

    Lecture 2 - Additional changes
    Lecture 2 - Additional changes

--- app/src/main/java/com/ltu/m7019e/m7019e_moviedbapp/MovieListFragment.kt
+++ app/src/main/java/com/ltu/m7019e/m7019e_moviedbapp/MovieListFragment.kt
@@ -5,23 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
 import com.ltu.m7019e.m7019e_moviedbapp.database.Movies
-import com.ltu.m7019e.m7019e_moviedbapp.databinding.FragmentFirstBinding
-import com.ltu.m7019e.m7019e_moviedbapp.utils.Constants
+import com.ltu.m7019e.m7019e_moviedbapp.databinding.FragmentMovieListBinding
+import com.ltu.m7019e.m7019e_moviedbapp.databinding.MovieListItemBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MovieListFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentMovieListBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -29,9 +23,15 @@ class FirstFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie_list, container, false)
         val movies = Movies()
-        binding.movies = movies
+
+        movies.list.forEach { movie ->
+            val movieListItemBinding: MovieListItemBinding = DataBindingUtil.inflate(inflater, R.layout.movie_list_item, container, false)
+            movieListItemBinding.movie = movie
+
+            binding.movieListLl.addView(movieListItemBinding.root)
+        }
 
         return binding.root
     }
